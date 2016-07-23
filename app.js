b0883491f85a5a849176d2f304b1757ffe7664fa
Paula <herrra1402@gmail.com>
2016-07-23 15:15:19 +0200

    Praktyczny projekt 1 - Losowanie Dużego Lotka

--- app.js
+++ app.js
@@ -1968,10 +1968,62 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 
 
+/* *********************************  PRAKTYCZNY 1 - LOSOWANIE DUŻEGO LOTKA ********************************* */
+
+
+(function(){
 
 
 
 
+	var btn = document.querySelector('#getNumbers'),
+	output = document.querySelector('#showNumbers');
+
+
+
+	function getRandom(min, max) {
+
+		return Math.round(Math.random() * (max - min) + min);
+
+	}
+
+
+
+	function showRandomNumbers() {
+
+
+		var numbers = [],
+		random;
+
+
+		for(var i = 0; i < 6; i++) {
+
+			random = getRandom(1, 49);
+
+
+
+			while(numbers.indexOf(random) !== -1) {
+
+				random = getRandom(1, 49);
+				console.log('Powtórzyła się liczba ' + random);
+
+			}
+
+
+			numbers.push(random);
+
+		}
+
+		output.value = numbers.join(', ');
+		//console.log(numbers);
+
+	}
+
+
+	btn.onclick = showRandomNumbers;
+
+
+})();
 
 
 
