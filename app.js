17fadf32ce7d9e2f5ec3dde4a886dc6a9bf26765
Paula <herrra1402@gmail.com>
2016-08-08 22:35:19 +0200

    Sortowanie tabeli po kolumnach

--- app.js
+++ app.js
@@ -1969,7 +1969,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 
 /* *********************************  PRAKTYCZNY 1 - LOSOWANIE DUŻEGO LOTKA ********************************* */
-
+/*
 
 (function(){
 
@@ -2025,13 +2025,98 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 })();
 
+*/
+
+
+/* **************************  PRAKTYCZNY 2 - SORTOWANIE TABELI PO KOLUMNACH CZ. 1 i 2 *************************** */
+
+(function() {
+
+	var table = document.querySelector('#myTable'),
+	ths = table.querySelectorAll('thead th'),
+	trs = table.querySelectorAll('tbody tr');
+
+
+function makeArray(nodeList) {
+
+	var arr = [];
+
+	for(var i = 0; i < nodeList.length; i++){
+
+		arr.push(nodeList[i]);
+	}
+
+	return arr;
+
+}
+
+function clearClassName(nodeList){
+
+	for(var i = 0; i < nodeList.length; i++){
+		nodeList[i].className = '';
+	}
+
+}
+
+
+
+function sortBy(e) {
+
+	var target = e.target,
+		thsArr = makeArray(ths),
+		trsArr = makeArray(trs),
+		index = thsArr.indexOf(target),
+		df = document.createDocumentFragment(),
+		order = (target.className === '' || target.className === 'desc') ? 'asc' : 'desc';
+
+		console.log(order);
+
+
+	clearClassName(ths);
+
+
+	trsArr.sort(function(a, b){
+
+		var tdA = a.children[index].textContent,
+			tdB = b.children[index].textContent;
+
+
+		if(tdA < tdB) {
+			return order === 'asc' ? -1 : 1;
+		} else if(tdA > tdB){
+			return order === 'asc' ? 1 : -1;
+		} else {
+			return 0;
+		}
+
+	});
+
+	trsArr.forEach(function(tr){
+
+		df.appendChild(tr);
+
+	});
+
+
+	target.className = order;
 
 
 
 
+	table.querySelector('tbody').appendChild(df);
+
+}
+
+
+for(var i = 0; i < ths.length; i++) {
+
+	ths[i].onclick = sortBy;
+
+}
 
 
 
+})();
 
 
 
