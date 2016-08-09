21230ac7ff2b0c38c7cfb15b91591c0c0876ff6b
Paula <herrra1402@gmail.com>
2016-08-09 23:52:28 +0200

    Walidacja formularza cz. 1 i 2

--- app.js
+++ app.js
@@ -2029,7 +2029,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 
 /* **************************  PRAKTYCZNY 2 - SORTOWANIE TABELI PO KOLUMNACH CZ. 1 i 2 *************************** */
-
+/*
 (function() {
 
 	var table = document.querySelector('#myTable'),
@@ -2119,12 +2119,124 @@ for(var i = 0; i < ths.length; i++) {
 })();
 
 
+*/
+
+
+
+/* **************************  PRAKTYCZNY 3 - WALIDACJA FORMULARZA CZ. 1 i 2 *************************** */
+
+(function(){
+
+	var form = document.querySelector('#myForm'),
+	fields = form.querySelectorAll('[data-error]');
+
+
+
+
+	function isNotEmpty(field){
+
+		return field.value !== '';
+	}
+
+
+
+	function isAtLeast(field, min){
+
+		return field.value.length >= min;
+
+	}
+
+
+
+	function isEmail(field){
+
+		return field.value.indexOf('@') !== -1;
+
+	}
+
+
+
+	function displayErrors(errors) {
+
+		var ul = document.querySelector('ul.errors');
+
+		if(!ul){
+			ul = document.createElement('ul');
+
+			ul.classList.add('errors');
+		}
+
+
+		ul.innerHTML = '';
+
+
+
+		errors.forEach(function(error){
+			var li = document.createElement('li');
+
+			li.textContent = error;
+
+			ul.appendChild(li);
+		});
+
+
+		form.parentNode.insertBefore(ul, form);
+
+	}
 
 
 
 
 
+	form.addEventListener('submit', function(e) {
 
+		e.preventDefault();
+
+		var errors = [];
+
+
+		for (var i = 0; i < fields.length; i++) {
+			
+			var field = fields[i],
+				isValid = false;
+
+			if(field.type === 'text'){
+				isValid = isNotEmpty(field);
+
+			} else if(field.type === 'email'){
+				isValid = isEmail(field);
+
+			} else if(field.type === 'select-one'){
+				isValid = isNotEmpty(field);
+
+			} else if(field.type === 'textarea'){
+				isValid = isAtLeast(field, 2);
+
+			}
+
+
+			if(!isValid){
+				field.classList.add('error');
+				errors.push(field.dataset.error);
+			} else {
+				field.classList.remove('error');
+			}
+
+		}
+
+if(errors.length){
+	displayErrors(errors);
+} else {
+	form.submit();
+}
+
+
+
+		console.log(errors);
+
+	}, false);
+
+})();
 
 
 
