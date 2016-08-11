9dd639fc5f6e4948629b2390f9b209ef05226374
Paula <herrra1402@gmail.com>
2016-08-11 23:31:33 +0200

    Tooltip

--- app.js
+++ app.js
@@ -2242,7 +2242,7 @@ if(errors.length){
 
 /* **************************  PRAKTYCZNY 4 - POWRÓT DO GÓRY STRONY *************************** */
 
-
+/*
 (function(){
 
 
@@ -2298,25 +2298,99 @@ if(errors.length){
 			button.classList.add('hidden');
 		}
 
-
-
-
 	}, false);
-
-
-
-
-
-
-
 
 })();
 
 
 
+/* **************************  PRAKTYCZNY 5 - TOOLTIP *************************** */
+
+
+(function() {
+
+
+	var globalTooltip = null;
 
 
 
+
+	function assignEvents(elems, type, event) {
+
+		for (var i = 0; i < elems.length; i++) {
+
+			elems[i].addEventListener(type, event, false);
+
+		}
+
+	}
+
+
+
+	function createTooltip(text, options) {
+
+		var tooltip = document.createElement('div');
+
+		tooltip.className = 'tooltip hidden';
+		tooltip.appendChild(document.createTextNode(text));
+		document.body.appendChild(tooltip);
+
+		tooltip.style.left = (options.x + options.w / 2) - (tooltip.offsetWidth / 2) +'px';
+		tooltip.style.top = (options.y - tooltip.offsetHeight) - 15 + 'px';
+		
+
+		tooltip.classList.remove('hidden');
+
+		globalTooltip = tooltip;
+
+
+	}
+
+
+
+	function showTooltip(e){
+
+		var options = {
+			w: e.target.offsetWidth,
+			x: e.target.offsetLeft,
+			y: e.target.offsetTop
+		};
+
+
+
+		var text = e.target.getAttribute('title');
+
+		createTooltip(text, options);
+
+		e.target.removeAttribute('title');
+
+	}
+
+
+
+	function removeTooltip(e){
+
+		e.target.setAttribute('title', globalTooltip.textContent);
+
+		globalTooltip.parentNode.removeChild(globalTooltip);
+
+	}
+
+
+
+	function init(elems){
+
+		assignEvents(elems, 'mouseenter', showTooltip);
+
+		assignEvents(elems, 'mouseleave', removeTooltip);
+
+	}
+
+
+
+	window.t00ltip = init;
+
+})();
 
 
 
